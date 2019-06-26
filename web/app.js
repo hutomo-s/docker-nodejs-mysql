43ba6a78ad9181312d5dfb5090ef0f6563140b89
Hutomo Sugianto <hutomo@premiro.com>
2019-06-26 07:29:38 +0700

    change mysql connection in app.js

--- web/app.js
+++ web/app.js
@@ -2,8 +2,24 @@
 const express = require('express');
 const app = express();
 
+const mysql = require("mysql");
+
 app.get('/', (req, res) => {
-    return res.send('Hello world 1');
+
+    const connection = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        charset: 'utf8'
+    })
+
+    connection.connect( (err) => {
+        if(err) {
+            return res.send('Error Connecting To DB');
+        }
+        return res.send('Success Connecting To DB. Hello world 1');
+    })
 });
 
 const PORT = process.env.PORT || 3000;
