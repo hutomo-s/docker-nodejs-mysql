e4799a9bae24a2d926a65e4d648c3123a36c663b
Hutomo Sugianto <hutomo@premiro.com>
2019-06-26 07:32:59 +0700

    add port

--- web/app.js
+++ web/app.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
 
     const connection = mysql.createConnection({
         host: process.env.DATABASE_HOST,
+        port: process.env.MYSQL_PORT,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
